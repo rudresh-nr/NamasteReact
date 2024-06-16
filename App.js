db4e342fdc5632a1bb0a71be2d13ca264492cdf3
rudresh-nr <rudresh9nr@gmail.com>
2024-06-16 12:23:16 +0530

    Components Introduction

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
-// How to create nested tags?
-const heading = React.createElement("h1",{id:"head"},["This is h1",
-                React.createElement("h2",{id:"smal-head"},"This is h2")]);
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+// JSX (transpiled before it reaches the JS) - PARCEL - Babel
+// JSX => Babel transpiles it to React.createElement  => ReactElement-JS Object => HTMLElement(render)
+
+//React Element
+const jsxheading = (<h1 className="head" tableIndex="5">
+    Namaste React Using JSX
+    </h1>);
+
+/* ......................React Components..........................*/
+
+// 1.Class Based Components - Old
+//Systax1
+const HeadingComponent1 = () => {
+    return <h1>Namaste React Functional Component Systax1</h1>
+}
+//Syntax2 && Component Composistion
+const HeadingComponent2 = () => (
+    <div>
+        <HeadingComponent1/>
+        <h1>Namaste React Functional Component Syntax2 without return keyword</h1>
+    </div>
+    );
 
 
-
-
-// React Need Root for DOM manipulation
+// 2.Functional Based Components - New
+console.log(jsxheading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
-
-console.log(heading);
+root.render(< HeadingComponent2/>);
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # NamasteReact
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - Written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification, Bundling, Compress
+- Consistent Hashing, Code Splitting
+- Differential Bundling - Compatible with all old and new browsers
+- Diagnostics & Error Handling
+- Host of HTTPS, SSL, HHTP
+- Tree Shaking Algorithm - Remove unused code
+- Different dev and prod bundles
+- Read More - https://parceljs.org/
+
+# Browsers List
+- https://browserslist.dev/
+- "browserslist":["last 2 Chrome versions"]
